b1a7fd42427b4f02d33f7e41f43d41e2c741efa3
Marcos Maues <yboostx@gmail.com>
2025-02-28 12:42:01 -0300

    28.02.25

--- segunda-aula-java/exercicio-12.java
+++ segunda-aula-java/exercicio-12.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
-class Main{
-    public static void main(String[], args){
-        
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
     }
 }

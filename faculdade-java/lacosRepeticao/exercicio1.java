80049e560dff28a65d71cf5ac6f1cb72963426e0
Marcos Maues <yboostx@gmail.com>
2025-02-28 13:43:32 -0300

    28.02.25

---
+++ faculdade-java/lacosRepeticao/exercicio1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Contador{
+    public static void main(String args){
+
+        for(int i = 1; i <= 10; i++){
+            System.out.printf("%d", 1);
+        }
+}
+ }
+

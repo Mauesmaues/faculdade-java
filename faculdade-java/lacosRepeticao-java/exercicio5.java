f49962910a61c1e0df375c3a0f1e64bf28b89e0e
Marcos Maues <yboostx@gmail.com>
2025-03-02 14:15:48 -0300

    28.02.25

---
+++ faculdade-java/lacosRepeticao-java/exercicio5.java
@@ -0,0 +1,15 @@
+/*Escreva um programa que apresente na tela a tabuada de um número lido do teclado. Por exemplo,
+considerando que o número lido é 2, o formato da apresentação deve ser o seguinte*/
+import java.util.Scanner;
+class Exercicio5{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite um numero pra receber a tatuaba de 1 a 10 dele;");
+        int numero = input.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            int tabuada = numero * i;
+            System.out.printf("%d x %d = %d\n", numero, i, tabuada);
+        }
+    }
+}
---
+++ faculdade-java/lacosRepeticao-java/exercicio6.java
@@ -0,0 +1,29 @@
+/*Escreva um programa que escreva os 10 primeiros números da sequência de Fibonacci (1, 1, 2, 3, 5, 8,
+13, 21, 34, 55).*/
+
+class Exercicio6{
+    public static void main(String[] args){
+        int numeroOne = 0;
+        int numeroTwo = 0;
+        int sequencia = 0;
+        for(int i = 1; i <= 10; i++){
+            if(numeroOne == 0){
+                numeroOne = i;
+                System.out.printf("%d,", numeroOne);
+            }else if(numeroTwo == 0){
+                numeroTwo = numeroOne;
+                System.out.printf("%d,", numeroTwo);
+            }else{
+                if(i == 10){
+                    sequencia = numeroOne + numeroTwo;
+                    System.out.printf("%d", sequencia);
+                }else {
+                    sequencia = numeroOne + numeroTwo;
+                    numeroOne = numeroTwo;
+                    numeroTwo = sequencia;
+                    System.out.printf("%d,", sequencia);
+                }
+            }
+        }
+    }
+}

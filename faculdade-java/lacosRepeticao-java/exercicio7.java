6700e39b7b5c0e8274b9599f451b086753d56ba4
Marcos Maues <yboostx@gmail.com>
2025-03-02 14:38:57 -0300

    28.02.25

---
+++ faculdade-java/lacosRepeticao-java/exercicio7.java
@@ -0,0 +1,19 @@
+/*Escreva um programa que mostre o resultado da série: 1/100 + 2/99 + 3/98 + 4/97 + ... + 100/1*/
+class SerieDivididoPor100{
+    public static void main(String[] args){
+        int divisor = 100, divisao = 0, soma = 0;
+        for(int i = 1; i <= 100; i++){
+            if(i == 100) {
+                divisao = i / divisor;
+                soma += divisao;
+                System.out.printf("%d / %d + ", i, divisor);
+                System.out.printf("= %d", soma);
+            }else{
+                divisao = i / divisor;
+                soma += divisao;
+                System.out.printf("%d / %d + ", i, divisor);
+                divisor--;
+            }
+        }
+    }
+}

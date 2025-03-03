f3536ae1e7bf83d77829b1cd8ab909dd787af69d
Marcos Maues <yboostx@gmail.com>
2025-03-03 13:35:50 -0300

    28.02.25

--- faculdade-java/matrizes-java/exercicio12.java
+++ faculdade-java/matrizes-java/exercicio12.java
@@ -1,12 +1,22 @@
 /*Escreva um programa que alimente uma matriz de 2 x 3 elementos inteiros e, utilizando estruturas de
 repetição, apresente na tela a soma de todos os elementos da matriz*/
 import java.util.Scanner;
+import java.util.Random;
 class MatrisAlimentando{
-    public static void main(String args){
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        Random numeroRandom = new Random();
         int matriz[][]= new int[3][4];
         for(int i = 0; i < matriz.length; i++){
-
+            for(int j = 0; j < matriz[0].length; j++){
+                matriz[i][j] = numeroRandom.nextInt(15);
+            }
+        }
+        for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[0].length; j++){
+                System.out.printf("%d | ", matriz[i][j]);
+            }
+                System.out.print("\n");
         }
     }
 }
